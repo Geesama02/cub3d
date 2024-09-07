f050314fbbbea0bdce943dca8b65266500411a8b
Oussama <oussama.thecreative@gmail.com>
2024-09-07 17:16:07 +0100

    edit

--- start_game.c
+++ start_game.c
@@ -6,7 +6,7 @@
 /*   By: oait-laa <oait-laa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/29 13:27:24 by oait-laa          #+#    #+#             */
-/*   Updated: 2024/09/07 14:00:29 by oait-laa         ###   ########.fr       */
+/*   Updated: 2024/09/07 17:14:28 by oait-laa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,7 +46,7 @@ void draw_line(t_vars *vars, int x1, int y1, int x2, int y2, int c)
 int	is_wall(t_vars *vars, int x, int y)
 {
 	printf("y wall -> %d | x wall -> %d\n", y, x);
-	mlx_put_pixel(vars->win, x, y, 0xFF0000FF);
+	// mlx_put_pixel(vars->win, x, y, 0xFF0000FF);
 	int x_axis = x / (32);
 	int y_axis = y / (32);
 	return (vars->map[y_axis][x_axis] == '1');
@@ -64,50 +64,71 @@ void	get_v_intersect(t_vars *vars, double rotation)
 {
 	int my_x = vars->character.frames[0].img->instances[0].x;
 	int my_y = vars->character.frames[0].img->instances[0].y;
-	double x_intersect = (my_x / 32) * 32;
+	double x_intersect = ((my_x / 32) * 32);
 	double y_intersect = 0;
 	double view_field = 60 * (M_PI / 180);
 	double rays = fmod((rotation - (view_field / 2)), M_PI * 2);
 	if (rays < 0)
-		rays += 2 * M_PI; 
+		rays += 2 * M_PI;
 	if (rays < 0.5 * M_PI || rays > 1.5 * M_PI)
-		x_intersect += 32;
+		x_intersect += (32);
 	if (fabs(tan(rays)) < 0.000001)
 		rays += 0.0001;
 	y_intersect = my_y + ((x_intersect - my_x) * tan(rays));
 	double next_x = 32;
 	if (!(rays < (0.5 * M_PI) || rays > (1.5 * M_PI)))
 		next_x *= -1;
-	double next_y = tan(rays) * 32;
+	double next_y = (tan(rays) * 32);
 	if (rays > M_PI && next_y > 0)
 		next_y *= -1;
 	if (rays <= M_PI && next_y < 0)
 		next_y *= -1;
-	double v_x = x_intersect * 0.25;
-	double v_y = y_intersect * 0.25;
+	double v_x = x_intersect;
+	double v_y = y_intersect;
 	if (!(rays < (0.5 * M_PI) || rays > (1.5 * M_PI)))
-		v_x--;
+		v_x--; 
 	while (v_x < vars->s_width && v_y < vars->s_height && v_x > 0 && v_y > 0)
 	{
 		if (is_wall(vars, v_x, v_y))
 			break;
-		v_y += (next_y * 0.25);
-		v_x += (next_x * 0.25);
+		v_y += (next_y);
+		v_x += (next_x);
 	}
-	// printf("len -> %f\n", get_line_len(vars, vars->wall_x, vars->wall_y));
+	printf("len -> %f\n", get_line_len(vars, vars->wall_x, vars->wall_y));
 	if (vars->wall_x < INT_MAX)
 		mlx_put_pixel(vars->win, vars->wall_x, vars->wall_y, 0x00FF00FF);
-	// printf("len 2-> %f\n", get_line_len(vars, v_x, v_y));
-	// printf("v_x -> %f | v_y -> %f\n", v_x, v_y);
-	// printf("h_x -> %f | h_y -> %f\n", vars->wall_x, vars->wall_y);
-	if (get_line_len(vars, vars->wall_x, vars->wall_y) > get_line_len(vars, v_x, v_y) && v_x < vars->s_width && v_y < vars->s_height && v_x > 0 && v_y > 0)
+	printf("len 2-> %f\n", get_line_len(vars, v_x, v_y));
+	printf("v_x -> %f | v_y -> %f\n", v_x, v_y);
+	printf("h_x -> %f | h_y -> %f\n", vars->wall_x, vars->wall_y);
+	if (vars->wall_x == 2147483647.000000)
 	{
 		if (!(rays < (0.5 * M_PI) || rays > (1.5 * M_PI)))
 			vars->wall_x = v_x + 1;
 		else
 			vars->wall_x = v_x;
 		vars->wall_y = v_y;
+		// return ;
 	}
+	if ((get_line_len(vars, vars->wall_x, vars->wall_y) > get_line_len(vars, v_x, v_y)) && v_x < vars->s_width && v_y < vars->s_height && v_x > 0 && v_y > 0)
+	{
+		printf("inside\n");
+		if (!(rays < (0.5 * M_PI) || rays > (1.5 * M_PI)))
+			vars->wall_x = v_x + 1;
+		else
+			vars->wall_x = v_x;
+		vars->wall_y = v_y;
+	}
+	if (v_x <= 0)
+	{
+		vars->wall_x = 0;
+		vars->wall_y = v_y;
+	}
+	else if (v_y <= 0)
+	{
+		vars->wall_x = v_x;
+		vars->wall_y = 0;
+	}
+		
 	
 }
 
@@ -129,27 +150,27 @@ void	get_h_intersect(t_vars *vars, double rotation)
 	double next_y = 32;
 	if (rays > M_PI) 
 		next_y *= -1;
-	double next_x = 32 / tan(rays);
+	double next_x = (32 / tan(rays));
 	if ((rays < (0.5 * M_PI) || rays > (1.5 * M_PI)) && next_x < 0)
 		next_x *= -1;
 	else if (!(rays < (0.5 * M_PI) || rays > (1.5 * M_PI)) && next_x > 0)
 		next_x *= -1;
-	double h_x = x_intersect * 0.25;
-	double h_y = y_intersect * 0.25;
+	double h_x = x_intersect;
+	double h_y = y_intersect;
 	if (rays > M_PI)
 		h_y--;
 	while (h_x < vars->s_width && h_y < vars->s_height && h_x > 0 && h_y > 0)
 	{
 		if (is_wall(vars, h_x, h_y))
 			break;
-		h_y += (next_y * 0.25);
-		h_x += (next_x * 0.25);
+		h_y += (next_y);
+		h_x += (next_x);
 	}
 	if (h_x < vars->s_width && h_y < vars->s_height && h_x > 0 && h_y > 0)
 	{
 		vars->wall_x = h_x;
 		if (rays > M_PI)
-			vars->wall_y = h_y + 1;
+			vars->wall_y = (h_y + 1);
 		else
 			vars->wall_y = h_y;
 	}
@@ -163,7 +184,7 @@ void	get_h_intersect(t_vars *vars, double rotation)
 void	key_hook(mlx_key_data_t keydata, void *vars)
 {
 	t_vars *my_vars = vars;
-	int rays_num = my_vars->s_width / 10;
+	int rays_num = my_vars->s_width;
 	double view_field = 60 * (M_PI / 180);
 	int my_x = my_vars->character.frames[0].img->instances[0].x;
 	int my_y = my_vars->character.frames[0].img->instances[0].y;
@@ -222,8 +243,8 @@ void	key_hook(mlx_key_data_t keydata, void *vars)
 			get_v_intersect(my_vars, ray_angle);
 			printf("my_vars->wall_x -> %f\n", my_vars->wall_x);
 			printf("my_vars->wall_y -> %f\n", my_vars->wall_y);
-			// mlx_put_pixel(my_vars->win, my_vars->wall_x, my_vars->wall_y, 0xFF0000FF);
-			draw_line(vars, my_x, my_y, my_vars->wall_x, my_vars->wall_y, 0xFF0000FF);
+			// mlx_put_pixel(my_vars->win, my_vars->wall_x, my_vars->wall_y, 0xFF0000FF); 
+			draw_line(vars, my_x + x_move, my_y + y_move, my_vars->wall_x, my_vars->wall_y, 0xFF0000FF);
 			i++;
 		}
 		// int i = 1;
@@ -237,7 +258,7 @@ void	key_hook(mlx_key_data_t keydata, void *vars)
 		// printf("width %d\n", my_vars->s_width);
 		// printf("height %d\n", my_vars->s_height);
 
-		// x_intersect = (my_x / 32) * 32;
+		// x_intersect = (my_x / 8) * 8;
 		// y_intersect = my_y + ((my_x - x_intersect) * tan(rays));
 		// mlx_put_pixel(my_vars->win, x_intersect, y_intersect, 0xFF0000FF);
 		// while (i < rays_num)
@@ -316,10 +337,10 @@ void	set_imgs(t_vars *vars)
 	mlx_texture_t *img = mlx_load_png("./imgs/floor.png");
 	mlx_texture_t *img2 = mlx_load_png("./imgs/wall.png");
 	mlx_texture_t *img3 = mlx_load_png("./imgs/ball.png");
-	// img->height = 32;
-	// img->width = 32;
-	// img2->height = 32;
-	// img2->width = 32;
+	// img->height = 8;
+	// img->width = 8;
+	// img2->height = 8;
+	// img2->width = 8;
 	img->height *= 0.25;
 	img->width *= 0.25;
 	img2->height *= 0.25;
